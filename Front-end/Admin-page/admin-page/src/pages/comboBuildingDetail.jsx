ce661cca0987ce86fe54657bc44cbccfe705de3e
Soul Breaker <SoulBreaker2k3@gmail.com>
2024-03-04 16:34:13 +0700

    test file name

--- Front-end/Admin-page/admin-page/src/pages/comboBuildingDetail.jsx
+++ Front-end/Admin-page/admin-page/src/pages/comboBuildingDetail.jsx
@@ -1,10 +1,10 @@
 import { Box, Typography, useTheme, Button } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
-import { tokens } from "../../theme";
+import { tokens } from "../theme";
 import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
 import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
 import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
-import Header from "../../components/Header";
+import Header from "../components/Header";
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link, useParams } from 'react-router-dom';
 import axios from 'axios';
