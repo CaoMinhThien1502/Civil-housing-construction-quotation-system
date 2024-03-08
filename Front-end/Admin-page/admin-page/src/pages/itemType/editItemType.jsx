80d79ec947f534919a5de3eec3d8370cb3ce4078
CaoMinhThien152002 <thien010502@gmail.com>
2024-03-08 09:22:29 +0700

    done edit item type

--- Front-end/Admin-page/admin-page/src/pages/itemType/editItemType.jsx
+++ Front-end/Admin-page/admin-page/src/pages/itemType/editItemType.jsx
@@ -4,10 +4,163 @@ import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+
+const initialValues = {
+    itemTypeId: 0,
+    itemTypeName: "",
+    status: "",
+};
+
+const userSchema = yup.object().shape({
+    itemTypeName: yup.string().required("Item Type Name is required"),
+});
 
 const EditItemType = () => {
+    const isNonMobile = useMediaQuery("(min-width:600px)");
+    const navigate = useNavigate();
+    const {id} = useParams();
 
+    const [getItemType, setItemType] = useState({});
+    useEffect(() => {
+        const fetchMaterialTypeById = async () => {
+            try {
+                const response = await fetch(`http://localhost:8080/building/item-type/id?typeId=${id}`, {
+                    method: 'GET',
+                    headers: {
+                        // 'Access-Control-Allow-Origin': '*',
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                const data = await response.json();
+                setItemType(data);
+            } catch (error) {
+                console.error('Error fetching materials:', error);
+            }
+        };
+
+        fetchMaterialTypeById();
+    }, []); // Empty dependency array to fetch data only once on component mount
+
+    const formik = useFormik({
+        initialValues: {
+            itemTypeName: `${getItemType.itemTypeName}`,
+            status: `${getItemType.status}`,
+        },
+        enableReinitialize: true,
+
+        onSubmit: async (values) => {
+            console.log("Values in onSubmit:", values);
+            try {
+                const response = await fetch(`http://localhost:8080/building/item-type/update?itemTypeId=${id}`, {
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(values),
+                    credentials: 'include', // Include credentials for cross-origin requests
+                });
+
+                if (!response.ok) {
+                    throw new Error(`API request failed with status ${response.status}`);
+                } else {
+                    console.log('Edit successful:', values);
+                }
+                
+                // Handle successful (e.g., navigate to a different page, store user data)
+                window.alert('Item Type updated successfully');
+                navigate('/itemType');
+            } catch (error) {
+                console.error('Error during submit:', error);
+                // Handle submit errors (e.g., display an error message to the user)
+            }
+        },
+
+        validationSchema: userSchema,
+    });
+
+    const [anchorEl, setAnchorEl] = useState(null); // State to manage dropdown menu
+    const handleOpen = (event) => {
+        setAnchorEl(event.currentTarget); // Open dropdown on click
+      };
+    const handleClose = () => {
+    setAnchorEl(null); // Close dropdown on selection or outside click
+    };
+
+    const handleChanges = (event) => {
+        formik.setFieldValue("status", event.target.value);
+        console.log(event.target.value);
+    }
+
+    return (
+        <Box m="20px">
+            <Header title="Edit Item Type" subtitle="Edit an Existing Item Type" />
+            <Formik
+            onSubmit={formik.handleSubmit}
+            initialValues={initialValues}
+            >
+                {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
+                    <form onSubmit={handleSubmit}>
+                        <Box 
+                        display="grid" 
+                        gap="30px" 
+                        gridTemplateColumns="repeat(4, minmax(0, 1fr))" // split the grid into 4 columns, each can have a minimum width of 0 and a maximum width of 1fr
+                        sx={{
+                            "& > div": { gridColumn: isNonMobile ? undefined : "span 4"}, // if the screen is non-mobile, then the grid column will be span 4
+                        }}
+                        >
+                            <TextField
+                            fullWidth
+                            variant="filled"
+                            type="text"
+                            label="Item Type Name"
+                            onBlur={handleBlur}
+                            onChange={formik.handleChange}
+                            value={formik.values.itemTypeName}
+                            name="itemTypeName"
+                            error={!!touched.itemTypeName && !!errors.itemTypeName}
+                            helperText={touched.itemTypeName && errors.itemTypeName}
+                            sx={{ gridColumn: "span 4" }}
+                            />
+                            <Typography sx={{ gridColumn: "span 4" }} variant="h6" gutterBottom>
+                                Previous Status: {formik.initialValues.status === "true" ? "Active" : "Inactive"}
+                            </Typography>
+                        </Box>
+                        <Box sx={{ gridColumn: "span 4" }}>
+                            <Select
+                                labelId="material-type-status-label"
+                                id="material-type-status"
+                                defaultValue="" 
+                                onChange={handleChanges}
+                                error={!!touched.itemTypeId && !!errors.itemTypeId} // Add error logic
+                                open={Boolean(anchorEl)} // Open dropdown based on state
+                                onClose={handleClose} // Close dropdown on selection or outside click
+                                onOpen={handleOpen} // Open dropdown on click
+                                fullWidth={true}
+                            >
+                                <MenuItem key={true} value={true}>
+                                    Active
+                                </MenuItem>
+                                <MenuItem key={false} value={false}>
+                                    Inactive
+                                </MenuItem>
+                            </Select>
+                        </Box>
+                        
+                        <Box display="flex" justifyContent="end" mt="20px">
+                            <Button onClick={() => navigate("/materialType")} color="secondary" variant="contained">
+                                Cancel
+                            </Button>
+                            <Box ml="10px"/>
+                            <Button type="submit" color="secondary" variant="contained">
+                                Edit Material
+                            </Button>
+                        </Box>
+                        
+                    </form>
+                )}
+            </Formik>
+        </Box>
+    )
 }
 
 export default EditItemType;
