90e12a5d5d9d3f9ad9479115892efc7a0600e016
KuongGB <cuong3right@gmail.com>
2024-03-10 10:11:40 +0700

    fix API request

--- Back-end/src/main/java/com/example/system/controller/RequestContractController.java
+++ Back-end/src/main/java/com/example/system/controller/RequestContractController.java
@@ -39,9 +39,9 @@ public class RequestContractController {
         return ResponseEntity.ok(dto);
     }
 
-    @GetMapping("/request-contract/list/id")
-    public ResponseEntity<List<RequestContractDto>> getRequestContractsById(Long userid){
-        List<RequestContractDto> list = requestContractService.findDtosByUserId(userid);
+    @GetMapping("/request-contract/list/email")
+    public ResponseEntity<List<RequestContractDto>> getRequestContractsByEmail(String email){
+        List<RequestContractDto> list = requestContractService.findDtosByEmail(email);
         return ResponseEntity.ok(list);
     }
     @PostMapping("/request-contract/create")
